d8f0bf5a5c1c20b777e30654cf63b1acc197e0f9
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-10 19:11:01 -0800

    add draft html text, form, and event listener for form

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const key = config.API_KEY;
 
-fetch(
-  `https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&ingredients=apples,+flour,+sugar&number=2`,
-  {
+function fetchData(e) {
+  fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&`, {
     headers: { 'Content-Type': 'application/json' },
-  }
-)
-  .then((res) => res.json())
-  .then((data) => renderData(data));
+  })
+    .then((res) => res.json())
+    .then((data) => renderData(data));
+}
 
 function renderData(data) {
   const recipes = data.results;
   console.log(recipes);
 }
+
+const form = document.getElementById('recipe-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetchData(e);
+});
