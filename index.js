7e82af63c1be71694b5cf0a573fd8067bcbb3bed
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-10 18:58:29 -0800

    basic fetch functionality

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const key = config.API_KEY;
+
+fetch(
+  `https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&ingredients=apples,+flour,+sugar&number=2`,
+  {
+    headers: { 'Content-Type': 'application/json' },
+  }
+)
+  .then((res) => res.json())
+  .then((data) => renderData(data));
+
+function renderData(data) {
+  const recipes = data.results;
+  console.log(recipes);
+}
