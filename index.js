b45a6a12fee92c43a7b978eb128cbd8ed591cbd0
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-10 19:38:19 -0800

    make ingredient form append to fetch URL properly

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const key = config.API_KEY;
 
 function fetchData(e) {
-  fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&`, {
+  let ingredients = '&ingredients=';
+  const formIngredients = document.getElementById('ingredients').value;
+  const ingredientArray = formIngredients.split(' ');
+
+  for (let i = 0; i < ingredientArray.length; i++) {
+    if (i === 0) {
+      ingredients += `${ingredientArray[0]},`;
+    } else if (i === ingredientArray.length - 1) {
+      ingredients += `+${ingredientArray[i]}`;
+    } else {
+      ingredients += `+${ingredientArray[i]},`;
+    }
+  }
+
+  const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${key}${ingredients}&number=2`;
+  console.log(apiUrl);
+  fetch(apiUrl, {
     headers: { 'Content-Type': 'application/json' },
   })
     .then((res) => res.json())
@@ -9,8 +25,7 @@ function fetchData(e) {
 }
 
 function renderData(data) {
-  const recipes = data.results;
-  console.log(recipes);
+  console.log(data);
 }
 
 const form = document.getElementById('recipe-form');
